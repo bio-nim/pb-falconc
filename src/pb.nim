0a428a80d4a5eae651778953dc9993b2ccc0da04
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-06-25 07:57:13 -0700

    welcome

--- src/pb.nim
+++ src/pb.nim
@@ -1,8 +1,10 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
-import os
+from os import nil
+from pbpkg/welcome import nil
 
 proc dataset(extras: seq[string]) =
     echo "pb dataset(" & $extras & ")"
+    echo welcome.getWelcomeMessage()
 
 when isMainModule:
     dataset(os.commandLineParams())
