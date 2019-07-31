a2670b009b0bf28bf2a5cd00a9bb010d1d25db97
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-07-30 23:04:14 -0700

    generate tests/falconc_unit_tests_junit.xml

--- tests/all.nim
+++ tests/all.nim
@@ -2,17 +2,14 @@
 import streams
 import unittest
 
-var f_of: File
-if not open(f_of, "falconc_unit_tests_junit.xml", fmWrite):
-    quit "Problem setting up report stream"
-var fs_of = newFileStream(f_of)
-var report_of = newJUnitOutputFormatter(fs_of)
+var fs_of = streams.newFileStream("falconc_unit_tests_junit.xml", fmWrite)
+var report_of = unittest.newJUnitOutputFormatter(fs_of)
 
 #This allows the tests to be printed to the junit report
-addOutputFormatter(report_of)
+unittest.addOutputFormatter(report_of)
 
 #This allows the tests to be printed to the console
-addOutputFormatter(defaultConsoleFormatter())
+unittest.addOutputFormatter(unittest.defaultConsoleFormatter())
 
 #The includes need to happen after we setup the report formatters
 
@@ -25,5 +22,5 @@ include "t_stats.nim"
 include "t_rotate.nim"
 
 #cleanup
-close(report_of)
-close(fs_of)
+unittest.close(report_of)
+streams.close(fs_of)
