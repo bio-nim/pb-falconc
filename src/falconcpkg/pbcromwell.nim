1474fbc48e028d5499a9685bc54be0293ba78bee
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-07-30 22:28:34 -0700

    pbcromwell.nim
    Try re for regular expressions.

---
+++ src/falconcpkg/pbcromwell.nim
@@ -0,0 +1,43 @@
+# vim: sw=4 ts=4 sts=4 tw=0 et:
+from ./util import nil
+from os import nil
+from re import nil
+from strutils import nil
+
+let re_raw_long = re.re(r"^raw_reads\.\d+\.raw_reads\.\d+\.las$")
+let re_p_long = re.re(r"^preads\.\d+\.preads\.\d+\.las$")
+
+let re_raw_short = re.re(r"^raw_reads\.\d+\.las$")
+let re_p_short = re.re(r"^preads\.\d+\.las$")
+
+proc should_remove*(fn: string): bool =
+    # For now, only check the base filename.
+    if not strutils.endsWith(fn, ".las"):
+        return false
+    let basename = os.extractFilename(fn)
+    if re.match(basename, re_raw_long):
+        return true
+    if re.match(basename, re_p_long):
+        return true
+    #let fi = os.getFileInfo(fn)
+    return false
+
+proc remove*(fn: string, dry_run: bool, verbose: bool=false) =
+    # Assume file exists. (Otherwise might raise.)
+    if verbose:
+        if not dry_run:
+            util.log("rm -f '" & fn & "'")
+        else:
+            util.log("#rm -f '" & fn & "'")
+    if not dry_run:
+        os.removeFile(fn)  # can raise
+
+proc remove_las*(run_dir: string, verbose: bool=true, dry_run: bool=true) =
+    ## So below
+    if verbose:
+        util.log("Deleting some las files under directory '" & run_dir & "' ...")
+        if dry_run:
+            util.log("(dry-run mode)")
+    for fn in util.walk(run_dir, relative=true):
+        if should_remove(fn):
+            remove(fn, verbose=verbose, dry_run=dry_run)
