ebefa62717e3fba5a805652b26b7670dbf3b8b52
Betty Li <bli@pacificbiosciences.com>
2019-08-23 15:30:22 -0700

    updating the bamboo specs so the tests show up and disabled xray task for now

--- bamboo-specs/src/main/java/pacbio/PlanSpec.java
+++ bamboo-specs/src/main/java/pacbio/PlanSpec.java
@@ -1,6 +1,7 @@
 package pacbio;
 
 import com.atlassian.bamboo.specs.api.BambooSpec;
+import com.atlassian.bamboo.specs.api.builders.AtlassianModule;
 import com.atlassian.bamboo.specs.api.builders.BambooKey;
 import com.atlassian.bamboo.specs.api.builders.BambooOid;
 import com.atlassian.bamboo.specs.api.builders.permission.PermissionType;
@@ -15,24 +16,28 @@ import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagemen
 import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
 import com.atlassian.bamboo.specs.api.builders.project.Project;
 import com.atlassian.bamboo.specs.api.builders.requirement.Requirement;
+import com.atlassian.bamboo.specs.api.builders.task.AnyTask;
 import com.atlassian.bamboo.specs.builders.task.CheckoutItem;
 import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.specs.builders.task.TestParserTask;
 import com.atlassian.bamboo.specs.builders.task.VcsCheckoutTask;
 import com.atlassian.bamboo.specs.builders.trigger.BitbucketServerTrigger;
 import com.atlassian.bamboo.specs.model.task.ScriptTaskProperties;
+import com.atlassian.bamboo.specs.model.task.TestParserTaskProperties;
 import com.atlassian.bamboo.specs.util.BambooServer;
+import com.atlassian.bamboo.specs.util.MapBuilder;
 
 @BambooSpec
 public class PlanSpec {
     
     public Plan plan() {
         final Plan plan = new Plan(new Project()
-    
-                .key(new BambooKey("SAT"))
+             
+                 .key(new BambooKey("SAT"))
                 .name("SMRT Analysis Tools (SAT)"),
             "falconc",
             new BambooKey("FALCONC"))
-            
+          
             .description("Plan created from Bamboo Java Specs,  modify http://bitbucket.pacificbiosciences.com:7990/projects/SAT/repos/falconc/browse project to update the plan.")
             .pluginConfigurations(new ConcurrentBuilds()
                     .useSystemWideDefault(false)
@@ -47,6 +52,18 @@ public class PlanSpec {
                                     .description("build")
                                     .location(ScriptTaskProperties.Location.FILE)
                                     .fileFromPath("bamboo_build.sh"))
+                            .finalTasks(new TestParserTask(TestParserTaskProperties.TestType.JUNIT)
+                                    .description("junitparsertry")
+                                    .resultDirectories("tests/falconc_unit_tests_junit.xml"),
+                                new AnyTask(new AtlassianModule("com.xpandit.plugins.xray-bamboo:cucumberFeaturesImportTask"))
+                                    .description("import falconc unit tests into X-ray")
+                                    .enabled(false)
+                                    .configuration(new MapBuilder()
+                                            .put("format", "")
+                                            .put("importServer", "2")
+                                            .put("inputInfoSwitcher", "")
+                                            .put("importFilePath", "tests/xray_summary.json")
+                                            .build()))
                             .requirements(new Requirement("system.os")
                                     .matchValue("linux")
                                     .matchType(Requirement.MatchType.EQUALS))))
@@ -66,15 +83,10 @@ public class PlanSpec {
     public PlanPermissions planPermission() {
         final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("SAT", "FALCONC"))
             .permissions(new Permissions()
-                    .userPermissions("cdunn", PermissionType.VIEW, PermissionType.BUILD, PermissionType.CLONE, PermissionType.EDIT, PermissionType.ADMIN)
-                    .userPermissions("zkronenberg", PermissionType.VIEW, PermissionType.BUILD, PermissionType.CLONE, PermissionType.EDIT, PermissionType.ADMIN)
-                    .userPermissions("bli", PermissionType.BUILD, PermissionType.CLONE, PermissionType.ADMIN, PermissionType.VIEW, PermissionType.EDIT));
+                    .userPermissions("cdunn", PermissionType.BUILD, PermissionType.CLONE, PermissionType.ADMIN, PermissionType.VIEW, PermissionType.EDIT)
+                    .userPermissions("zkronenberg", PermissionType.BUILD, PermissionType.CLONE, PermissionType.ADMIN, PermissionType.VIEW, PermissionType.EDIT)
+                    .userPermissions("bli", PermissionType.EDIT, PermissionType.BUILD, PermissionType.CLONE, PermissionType.VIEW, PermissionType.ADMIN));
         return planPermission;
-    }
-
-    public Plan createPlan() {
-    // TODO Auto-generated method stub
-        return null;
     }
     
     public static void main(String... argv) {
