d66874b7a74a5662f6f8639d499ffaec6632a766
Ivan Sovic <isovic@pacificbiosciences.com>
2020-01-08 04:27:43 -0800

    Implemented an overlap parser (overlapParser.nim) and added tests."

---
+++ src/falconcpkg/overlapParser.nim
@@ -0,0 +1,92 @@
+from strutils import split, parseInt, parseFloat, parseBool
+from strformat import fmt
+from ./util import isEmptyFile, log
+import streams
+
+type
+    Overlap* = object
+        Aname*: string
+        Bname*: string
+        score*: int
+        idt*: float64
+        Arev*: bool
+        Astart*: int
+        Aend*: int
+        Alen*: int
+        Brev*: bool
+        Bstart*: int
+        Bend*: int
+        Blen*: int
+        tag: string
+
+proc parseOverlap*(s: string): Overlap =
+    var ovl: Overlap
+    let ld = s.split(" ")
+    if ld.len() < 12 or ld.len() > 13:
+        let msg = "Error parsing ovl (split={ld.len()}): '{s}'".fmt
+        log(msg)
+    doAssert (ld.len() == 12 or ld.len() == 13)
+    ovl.Aname = ld[0]
+    ovl.Bname = ld[1]
+    ovl.score = parseInt(ld[2])
+    ovl.idt = parseFloat(ld[3])
+    ovl.Arev = parseBool(ld[4])
+    ovl.Astart = parseInt(ld[5])
+    ovl.Aend = parseInt(ld[6])
+    ovl.Alen = parseInt(ld[7])
+    ovl.Brev = parseBool(ld[8])
+    ovl.Bstart = parseInt(ld[9])
+    ovl.Bend = parseInt(ld[10])
+    ovl.Blen = parseInt(ld[11])
+    ovl.tag = ""
+    if ld.len() >= 13:
+        ovl.tag = ld[12]
+    return ovl
+
+proc toString*(o: Overlap): string =
+    var strandA = 0
+    var strandB = 0
+    var tag = ""
+    if o.Arev: strandA = 1
+    if o.Brev: strandB = 1
+    if o.tag != "": tag = " " & o.tag
+    result = "{o.Aname} {o.Bname} {o.score} {o.idt:.3f} {strandA} {o.Astart} {o.Aend} {o.Alen} {strandB} {o.Bstart} {o.Bend} {o.Blen}{tag}".fmt
+
+proc `$`(o: Overlap): string =
+    return toString(o)
+
+proc toTuple*(o: Overlap): (string, string, int, float64, bool, int, int, int, bool, int, int, int, string) =
+    return (o.Aname, o.Bname, o.score, o.idt, o.Arev, o.Astart, o.Aend, o.Alen, o.Brev, o.Bstart, o.Bend, o.Blen, o.tag)
+
+iterator getNextPile*(sin: Stream): seq[Overlap] =
+    var
+        readA: string
+        ov: Overlap
+        buff = ""
+        ovls = newSeq[Overlap]()
+
+    if readLine(sin, buff):
+        try:
+            ov = parseOverlap(buff)
+            ovls.add(ov)
+            readA = ov.Aname
+            while(readLine(sin, buff)):
+                ov = parseOverlap(buff)
+                if ov.Aname != readA:
+                    yield ovls
+                    readA = ov.Aname
+                    ovls = @[]
+                ovls.add(ov)
+            yield ovls
+        except AssertionError:
+            log("Error parsing line: buff='{buff}'".fmt)
+            raise
+
+proc parseM4*(sin: streams.Stream): seq[Overlap] =
+    for line in streams.lines(sin):
+        if strutils.startswith(line, '-'):
+            # This can be used to signify EOF, but only if
+            # we ever have more trouble with filesystem errors.
+            break
+        let overlap = parseOverlap(line)
+        result.add(overlap)
---
+++ tests/t_overlapParser.nim
@@ -0,0 +1,93 @@
+import unittest
+import falconcpkg/overlapParser as uut
+import streams
+from strformat import fmt
+
+let testDataGetNextPile1 = """
+15210 10043 -4436 99.910 0 8199 12654 16697 0 8427 12886 12886 u
+5600 10005 -639 90.833 0 1944 3264 9722 1 4349 5671 8935 u
+5600 10219 -6264 99.936 0 3437 9722 9722 1 245 6528 7272 u
+5600 10419 -639 90.833 0 1944 3264 9722 0 1877 3199 8492 u
+32757 10030 -88 70.513 0 9470 12284 16890 0 6365 9129 11852 u
+32757 10150 -88 70.581 0 9470 12284 16890 1 5052 7819 12414 u
+32757 10304 -576 89.165 0 14841 16710 16890 0 2004 3850 4474 u
+32757 10882 -822 96.935 0 7946 9088 16890 1 5172 6314 12879 u
+"""
+let testDataGetNextPile1Expected = [
+"""
+15210 10043 -4436 99.910 0 8199 12654 16697 0 8427 12886 12886 u
+""",
+"""
+5600 10005 -639 90.833 0 1944 3264 9722 1 4349 5671 8935 u
+5600 10219 -6264 99.936 0 3437 9722 9722 1 245 6528 7272 u
+5600 10419 -639 90.833 0 1944 3264 9722 0 1877 3199 8492 u
+""",
+"""
+32757 10030 -88 70.513 0 9470 12284 16890 0 6365 9129 11852 u
+32757 10150 -88 70.581 0 9470 12284 16890 1 5052 7819 12414 u
+32757 10304 -576 89.165 0 14841 16710 16890 0 2004 3850 4474 u
+32757 10882 -822 96.935 0 7946 9088 16890 1 5172 6314 12879 u
+"""
+]
+
+
+proc helperPileToString(pile: seq[uut.Overlap]): string =
+    for ov in pile:
+        result.add(uut.toString(ov))
+        result.add('\n')
+
+suite "overlapParser parseOverlap":
+    test "empty input":
+        let record= ""
+        expect Exception:
+            let parsed = uut.parseOverlap(record)
+
+    test "line with 12 fields":
+        let record = "000000001 000001050 -9662 98.670 0 0 9656 9656 1 626 10288 12247"
+        let parsed = uut.parseOverlap(record)
+        check uut.toString(parsed) == record
+
+    test "line with 13 fields":
+        let record = "000000001 000001050 -9662 98.670 0 0 9656 9656 1 626 10288 12247 contained"
+        let parsed = uut.parseOverlap(record)
+        check uut.toString(parsed) == record
+
+    test "wrong number of fields, less than 12":
+        let record = "000000001 000001050 -9662 98.670 0 0"
+        expect Exception:
+            let parsed = uut.parseOverlap(record)
+
+    test "wrong number of fields, more than 13":
+        let record = "000000001 000001050 -9662 98.670 0 0 9656 9656 1 626 10288 12247 contained extraField"
+        expect Exception:
+            let parsed = uut.parseOverlap(record)
+
+suite "overlapParser getNextBlock":
+    test "empty input":
+        let inStr = ""
+        let inStream: Stream = newStringStream(inStr)
+        var results: seq[string]
+        for pile in uut.getNextPile(inStream):
+            results.add(helperPileToString(pile))
+        check results.len() == 0
+
+    test "normal input":
+        let inStr = testDataGetNextPile1
+        let inStream: Stream = newStringStream(inStr)
+        var results: seq[string]
+        for pile in uut.getNextPile(inStream):
+            results.add(helperPileToString(pile))
+        check results == testDataGetNextPile1Expected
+
+suite "overlapParser parseM4":
+    test "empty input":
+        let inStr = ""
+        let inStream: Stream = newStringStream(inStr)
+        let results = uut.parseM4(inStream)
+        check helperPileToString(results) == inStr
+
+    test "normal input":
+        let inStr = testDataGetNextPile1
+        let inStream: Stream = newStringStream(inStr)
+        let results = uut.parseM4(inStream)
+        check helperPileToString(results) == inStr
