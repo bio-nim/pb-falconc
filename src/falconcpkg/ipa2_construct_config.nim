423d05bdc3e594476f5c33c0b1a7cb6a5ee26f0f
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-03-25 23:58:21 -0700

    IPA: falconc ipa2_construct_config
    Same as Python `ipa2/scripts/ipa2_construct_config`
    
    Note: `--no-sort` will maintain original order in the default_config.
    
    TAK-522

--- src/falconcpkg/ipa2_construct_config.nim
+++ src/falconcpkg/ipa2_construct_config.nim
@@ -1,13 +1,120 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
-from ./util import isEmptyFile, log
+#from ./util import isEmptyFile, log
 from os import quoteShell
+from strformat import `&`
+import json, tables, sets, sequtils, strutils, streams
 
-proc run*() =
-    echo "run"
+var default_config = """
+config_genome_size = 0
+config_coverage = 0
+config_polish_run = 1
+config_phase_run = 1
 
-proc main*(out_fn: string, out_fmt: string = "json") =
+config_existing_db_prefix =
+config_block_size = 4096
+config_seqdb_opt = --compression 0
+config_seeddb_opt = -k 32 -w 80 --space 2
+config_ovl_opt =
+config_ovl_min_idt = 98
+config_ovl_min_len = 1000
+config_ovl_filter_opt = --max-diff 80 --max-cov 100 --min-cov 1 --bestn 10 --min-len 4000 --gapFilt --minDepth 4
+config_use_seq_ids = 0
+config_phasing_opt =
+config_phasing_piles = 10000
+"""
+
+type
+  ConfigTable* = tables.OrderedTable[string, string]
+  #ConfigTuple = tuple
+  #  genome_size,
+  #    coverage,
+  #    polish_run,
+  #    phase_run,
+  #    existing_db_prefix,
+  #    block_size,
+  #    seqdb_opt,
+  #    seeddb_opt,
+  #    ovl_opt,
+  #    ovl_min_idt,
+  #    ovl_min_len,
+  #    ovl_filter_opt,
+  #    use_seq_ids,
+  #    phase_opt,
+  #    phasing_piles: string
+
+proc formatter_json(config_dict: ConfigTable): string =
+    return json.pretty(% config_dict, 4)
+
+proc formatter_bash(config_dict: ConfigTable): string =
+    var lines: seq[string]
+    for key in tables.keys(config_dict):
+        let val = quoteShell(config_dict[key])
+        # Accumulate the lines.
+        let line = &"{key}={val}"
+        lines.add(line)
+    let ret = strutils.join(lines, "\n")
+    return ret
+
+proc convert_config_to_dict*(config: string): ConfigTable =
+    # Replace newlines with a ';' to make it uniform. (Also to support ';' as separator.)
+    let in_config = strutils.replace(config, '\n', ';')
+
+    for line in strutils.split(in_config, ';'):
+        let stripped = strutils.strip(line)
+        if stripped == "": continue
+        let keyval = strutils.split(stripped, '=')
+        doAssert len(keyval) == 2, "Malformed config option. Each config option needs to have exactly one '=' character, specifying the config option on the left side, and the value on the right side. Line: " & $stripped
+        let
+          param_name = strutils.strip(keyval[0])
+          param_val = strutils.strip(keyval[1])
+        result[param_name] = param_val
+
+proc validate_param_names(valid_params, test_params: ConfigTable) =
+    let valid = sets.toHashSet(sequtils.toSeq(tables.keys(valid_params)))
+    var unknown_params: seq[string]
+    for p in tables.keys(test_params):
+        if not sets.contains(valid, p):
+            unknown_params.add(p)
+    if len(unknown_params) != 0:
+        let msg = "Unknown config parameters specified: " & $unknown_params
+        raiseAssert($unknown_params)
+
+proc parse*(in_str: string): ConfigTable =
+    # Load the defaults.
+    let default_config_dict = convert_config_to_dict(default_config)
+
+    # Load the user specified options.
+    let user_config_dict = convert_config_to_dict(in_str)
+
+    # Validate the user config. There shouldn't be any keys which do not
+    # appear in the default_config_dict.
+    validate_param_names(default_config_dict, user_config_dict)
+
+    # Update the defaults with user specified values.
+    result = default_config_dict
+    for k, v in tables.pairs(user_config_dict):
+        result[k] = v
+
+proc run*(fp_out, fp_in: streams.Stream, out_fmt: char, sort: bool) =
+    # Collect the input lines.
+    let in_str = strutils.join(sequtils.toSeq(lines(fp_in)), ";")
+
+    var config_dict = parse(in_str)
+    if sort:
+        # Sort to match Python behavior.
+        tables.sort(config_dict, system.cmp)
+
+    # Write the dict.
+    let formatter = if out_fmt == 'j': formatter_json else: formatter_bash
+    let out_str = formatter(config_dict)
+    fp_out.writeLine(out_str)
+
+proc main*(out_fn: string, out_fmt: string = "json", in_fn = "-", no_sort = false) =
     ## Takes an advanced options string, and reformats it into JSON format.
     ## Input/output is on stdin/stdout. Options which aren't set explicitly in the input
     ## will be set to default (configurable via args).
 
-    echo "Hi runner", out_fn, out_fmt
+    var fp_in = if in_fn=="-": streams.newFileStream(stdin) else: streams.newFileStream(in_fn, fmRead)
+    var fp_out = streams.newFileStream(out_fn, fmWrite)
+    run(fp_out, fp_in, out_fmt[0], not no_sort)
+    fp_out.close()
--- tests/t_ipa2.nim
+++ tests/t_ipa2.nim
@@ -1,8 +1,64 @@
 # vim: sw=4 ts=4 sts=4 tw=0 et:
 
 import falconcpkg/ipa2_construct_config
-import unittest
+import unittest, streams, tables
 
-test "welcome":
-    run()
-    #check run() == "Hello, World!"
+let expected_default_bash = """
+config_block_size=4096
+config_coverage=0
+config_existing_db_prefix=''
+config_genome_size=0
+config_ovl_filter_opt='--max-diff 80 --max-cov 100 --min-cov 1 --bestn 10 --min-len 4000 --gapFilt --minDepth 4'
+config_ovl_min_idt=98
+config_ovl_min_len=1000
+config_ovl_opt=''
+config_phase_run=1
+config_phasing_opt=''
+config_phasing_piles=10000
+config_polish_run=1
+config_seeddb_opt='-k 32 -w 80 --space 2'
+config_seqdb_opt='--compression 0'
+config_use_seq_ids=0
+"""
+
+let expected_default_json = """
+{
+    "config_block_size": "4096",
+    "config_coverage": "0",
+    "config_existing_db_prefix": "",
+    "config_genome_size": "0",
+    "config_ovl_filter_opt": "--max-diff 80 --max-cov 100 --min-cov 1 --bestn 10 --min-len 4000 --gapFilt --minDepth 4",
+    "config_ovl_min_idt": "98",
+    "config_ovl_min_len": "1000",
+    "config_ovl_opt": "",
+    "config_phase_run": "1",
+    "config_phasing_opt": "",
+    "config_phasing_piles": "10000",
+    "config_polish_run": "1",
+    "config_seeddb_opt": "-k 32 -w 80 --space 2",
+    "config_seqdb_opt": "--compression 0",
+    "config_use_seq_ids": "0"
+}
+"""
+
+suite "ipa2_construct_config":
+  test "write bash":
+    var
+        outs = streams.newStringStream()
+        ins = streams.newStringStream()
+    run(outs, ins, 'b', true)
+    outs.setPosition(0)
+    check outs.readAll() == expected_default_bash
+
+  test "write json":
+    var
+        outs = streams.newStringStream()
+        ins = streams.newStringStream()
+    run(outs, ins, 'j', true)
+    outs.setPosition(0)
+    check outs.readAll() == expected_default_json
+
+  test "override":
+    let cfg = parse("config_coverage=11")
+    check cfg["config_coverage"] == "11"
+    check cfg["config_block_size"] == "4096"
