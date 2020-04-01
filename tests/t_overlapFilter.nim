09eeb57fb70fec7f3b42fb56a2ecdec5551b39d6
Ivan Sovic <isovic@pacificbiosciences.com>
2020-04-01 06:21:36 -0700

    The overlapFilter is now using the overlapParser. Removed the duplicate code in the overlapFilter.

--- tests/t_overlapFilter.nim
+++ tests/t_overlapFilter.nim
@@ -1,5 +1,6 @@
 import unittest
 import falconcpkg/overlapFilter
+from falconcpkg/overlapParser import parseOverlap, getNextPile, toString
 from streams import nil
 from strformat import fmt
 
@@ -8,59 +9,19 @@ from strformat import fmt
  000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained
 ]#
 
-suite "overlapFilter parseOK":
-    let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained"
-    let parsed = parseOvl(record1)
-    test "ids":
-        check parsed.ridA == "000000001"
-        check parsed.ridB == "000001050"
-    test "score":
-        check parsed.score == -9662
-    test "idt":
-        check parsed.idt == 98.67
-    test "strands":
-        check parsed.strand1 == false
-        check parsed.strand2 == true
-    test "starts":
-        check parsed.start1 == 0
-        check parsed.start2 == 626
-    test "ends":
-        check parsed.end1 == 9656
-        check parsed.end2 == 10288
-    test "lengths":
-        check parsed.l1 == 9656
-        check parsed.l2 == 12247
-
-suite "overlapFilter extraColumns":
-    test "13":
-        let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained"
-        let parsed = parseOvl(record1)
-        check parsed.tag == "contained"
-        check parsed.tagplus == "contained"
-    test "14":
-        let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained foo"
-        let parsed = parseOvl(record1)
-        check parsed.tag == "contained"
-        check parsed.tagplus == "contained foo"
-    test "15":
-        let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained foo bar"
-        let parsed = parseOvl(record1)
-        check parsed.tag == "contained"
-        check parsed.tagplus == "contained foo bar"
-
 suite "overlapFilter containedOK":
     test "contained":
         let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check contained(parsed) == true
     test "notcontained":
         let record1 = "000000001 000001050 -9662 98.67 0 10 9656 9656 1 626 10288 12247 contained"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check contained(parsed) == false
 
 suite "overlapFilter idtOK":
     let record1 = "000000001 000001050 -9662 98.67 0 0 9656 9656 1 626 10288 12247 contained"
-    let parsed = parseOvl(record1)
+    let parsed = parseOverlap(record1)
     test "ranges":
         check lowIdt(parsed, 0) == false
         check lowIdt(parsed, 100) == true
@@ -71,37 +32,37 @@ suite "overlapFilter idtOK":
 suite "overlapFilter Fraction":
     test "lOverlap false":
         let record1 = "000000010 000000578 -13654 99.69 0 0 13668 15613 1 0 13654 16307 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check checkFractionOverlap(parsed, 10.0, 90.0) == false
     test "lOverlap true":
         let record1 = "000000010 000000578 -13654 99.69 0 0 15600 15613 1 0 13654 16307 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check checkFractionOverlap(parsed, 10.0, 90.0) == true
 
 suite "overlapFilter terminatedAlignments":
     test "readA pass start":
         let record1 = "000000001 000002664 -5006 98.58 0 0 5004 9656 1 0 5006 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == false
     test "readA pass end":
         let record1 = "000000001 000002664 -5006 98.58 0 10 9656 9656 1 0 5006 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == false
     test "readA fail":
         let record1 = "000000001 000002664 -5006 98.58 0 10 5004 9656 1 0 5006 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == true
     test "readB pass start":
         let record1 = "000000001 000002664 -5006 98.58 0 0 5004 9656 1 0 5006 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == false
     test "readB pass end":
         let record1 = "000000001 000002664 -5006 98.58 0 0 5004 9656 1 10 10117 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == false
     test "readB fail":
         let record1 = "000000001 000002664 -5006 98.58 0 0 5004 9656 1 10 5006 10117 overlap"
-        let parsed = parseOvl(record1)
+        let parsed = parseOverlap(record1)
         check missingTerminus(parsed) == true
 
 suite "overlapFilter m4filtContained":
@@ -212,26 +173,33 @@ let tp_15210 = stream("data/chimera/tp_15210.ovlp")
 
 suite "chimera filter":
     test "5600 tp marked":
-        for i in abOvl(tp_5600):
+        for i in getNextPile(tp_5600):
             check gapInCoverage(i, 4, 90.0) == true
     test "15210 tp marked":
-        for i in abOvl(tp_15210):
+        for i in getNextPile(tp_15210):
             check gapInCoverage(i, 4, 90.0) == true
     test "32757 fp not marked":
-        for i in abOvl(fp_32757):
+        for i in getNextPile(fp_32757):
             check gapInCoverage(i, 4, 90.0) == false
     test "17238 fp not marked":
-        for i in abOvl(fp_17238):
+        for i in getNextPile(fp_17238):
             check gapInCoverage(i, 4, 90.0) == false
     test "30455 FP not marked ":
-        for i in abOvl(fp_30455):
+        for i in getNextPile(fp_30455):
             check gapInCoverage(i, 4, 90.0) == false
     test "118816930 CCS tp marked":
-        for i in abOvl(fn_118816930):
+        for i in getNextPile(fn_118816930):
             check gapInCoverage(i, 4, 90.0) == true
     test "90965490 CCS tp marked":
-        for i in abOvl(fn_90965490):
+        for i in getNextPile(fn_90965490):
             check gapInCoverage(i, 4, 90.0) == true
     test "105513170 CCS FN, this should be marked, kept for future improvements":
-        for i in abOvl(fn_105513170):
+        for i in getNextPile(fn_105513170):
             check gapInCoverage(i, 4, 90.0) == false
+
+suite "m4filt workflow"
+    test "check sanity of output":
+        # TODO: the output needs to be checked against a simple example.
+        # It can be as simple as something that just passes through.
+        # This will probably require some refactoring, because the function operates
+        # on files and not streams.
