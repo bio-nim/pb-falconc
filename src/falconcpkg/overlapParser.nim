ec062448e5443e7ccad51f12ca958cebdeb9d45e
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-04-01 09:58:00 -0700

    pileup -> pile per isovic

--- src/falconcpkg/overlapParser.nim
+++ src/falconcpkg/overlapParser.nim
@@ -29,42 +29,42 @@ type
     M4Index* = seq[M4IndexRecord]
 
 
-iterator readNextPileup(f: streams.Stream, pileup: var string): int =
-    # A "pileup" is a block of newline-delimited lines which
+iterator readNextPile(f: streams.Stream, pile: var string): int =
+    # A "pile" is a block of newline-delimited lines which
     # each start with the same string.
-    # On each iteration, set "pileup" to that string and yield
-    # the number of lines in the pileup.
+    # On each iteration, set "pile" to that string and yield
+    # the number of lines in the pile.
     var
         curr: string = "" # Everything starts with this.
         line: string
         index = 0
         count = 0
-    pileup.setLen(0)
+    pile.setLen(0)
 
     while streams.readLine(f, line):
         if not strutils.startsWith(line, curr):
             yield count
             count = 0
-            pileup.setLen(0)
+            pile.setLen(0)
         if 0 == count:
-            assert 0 == pileup.len
+            assert 0 == pile.len
             let iSpace = strutils.find(line, ' ')
             assert iSpace != -1, line
             curr = line[0 .. iSpace]
-        pileup.add(line)
-        pileup.add("\n") # since it was stripped by readLine()
+        pile.add(line)
+        pile.add("\n") # since it was stripped by readLine()
         count += 1
     if 0 != count:
-        assert 0 != pileup.len
+        assert 0 != pile.len
         yield count
 
 proc index*(ovls_s: streams.Stream): M4Index =
     var pos: int64 = 0
-    var pileup: string
-    for count in readNextPileup(ovls_s, pileup):
-        let rec = M4IndexRecord(count: count.int32, pos: pos, len: pileup.len.int32)
+    var pile: string
+    for count in readNextPile(ovls_s, pile):
+        let rec = M4IndexRecord(count: count.int32, pos: pos, len: pile.len.int32)
         result.add(rec)
-        pos += pileup.len
+        pos += pile.len
 
 
 proc parseOverlap*(s: string): Overlap =
