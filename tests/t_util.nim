e82641c842c2fe3db0a995543fce0e668783fe44
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-04-04 07:57:54 -0700

    splitWeighted

--- tests/t_util.nim
+++ tests/t_util.nim
@@ -18,3 +18,19 @@ suite "util":
         check thousands(-1_000) == "-1,000"
         check thousands(-1) == "-1"
         check thousands(-0) == "0"
+    test "splitWeighted":
+        check splitWeighted(0, @[]) == []
+        check splitWeighted(0, @[42]) == []
+        check splitWeighted(1, @[42]) == [1]
+        check splitWeighted(2, @[42, 2]) == [1, 1]
+        check splitWeighted(3, @[1, 1, 1]) == [1, 1, 1]
+        check splitWeighted(3, @[1, 1, 1, 1]) == [2, 1, 1]
+        check splitWeighted(3, @[1, 1]) == [1, 1]
+        check splitWeighted(1, @[1, 2, 3, 4]) == [4]
+        check splitWeighted(2, @[1, 2, 3, 4]) == [3, 1]
+        check splitWeighted(3, @[1, 2, 3, 4]) == [3, 1] # greedy
+        check splitWeighted(4, @[1, 2, 3, 4]) == [2, 1, 1] # greedy, so order matters
+        check splitWeighted(4, @[4, 3, 2, 1]) == [1, 1, 1, 1] # see?
+        check splitWeighted(3, @[4, 3, 2, 1]) == [1, 1, 2]
+        check splitWeighted(2, @[4, 3, 2, 1]) == [2, 2]
+        check splitWeighted(1, @[4, 3, 2, 1]) == [4]
